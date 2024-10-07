b002efd7bfcc1db9ce8a9beefabc50ecf1741057
MinhPhuCoder <phuhackerchuyenhackff@gmail.com>
2024-10-07 17:28:05 +0700

    Create setup.py

---
+++ .github/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lmpcoder',
+    version='1',
+    packages=find_packages(),
+    install_requires=[],
+    description='Thư Viện Này Sẽ Giúp Bạn Thuận Tiện Hơn Khi Dùng Python Với Các Cú Pháp Ngắn Gọn Và Tiện Ích, Giúp Bạn Nhanh Hơn Trong Việc Viết Python Của Bạn',
+    author='Lâm Minh Phú',
+    author_email='zbllf2lollpll@gmail.com',
+    url='',
+)
